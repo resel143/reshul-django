0d1d8f85b6614c0449fb23339fb9df15c02257cc
Reshul Wate <reshulwate@gmail.com>
2026-01-12 18:25:12 +0530

    Feat: GET by ID api

--- newbackend/api/urls.py
+++ newbackend/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import get_book_list, add_book, update_book, delete_book
+from .views import get_book_list, add_book, update_book, delete_book, get_book_by_id
 
 urlpatterns = [
     path('books/', get_book_list, name='getBook'),
+    path('books/<int:book_id>', get_book_by_id, name='getBookById'),
     path('books/add', add_book, name='addBook'),
     path('books/update', update_book, name='updateBook'),
     path('books/delete', delete_book, name='deleteBook'),
